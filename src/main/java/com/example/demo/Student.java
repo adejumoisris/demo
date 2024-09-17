6df38fb6222c0976c3f3cabaeb6459a910d6c08a
deca <idrisadejumo@gmail.com>
2024-09-17 12:05:21 +0100

    initial commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    
+     private int id;
+      private  String name;
+     private String address;
+}
